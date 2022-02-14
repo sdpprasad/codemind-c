a718f9774490ad2cfa9234f6de0d7f57d0cd42c6
sdpprasad <96641595+sdpprasad@users.noreply.github.com>
2022-02-14 20:02:01 +0530

    #include<stdio.h> int reverse(int n) {     int rev=0,r;     while(n)     {         r=n%10;         rev=rev*10+r;         n=n/10;     }     return rev; } int main() {     int x;     scanf("%d",&x);     while(x)     {         int y;         y=reverse(x)+x;         if(y==reverse(y))         {             printf("%d",y);             break;
            }
            else
            x=y;
        }
        return 0;
        
    }

---
+++ Reverse_Palindrome.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int reverse(int n)
+{
+    int rev=0,r;
+    while(n)
+    {
+        r=n%10;
+        rev=rev*10+r;
+        n=n/10;
+    }
+    return rev;
+}
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    while(x)
+    {
+        int y;
+        y=reverse(x)+x;
+        if(y==reverse(y))
+        {
+            printf("%d",y);
+            break;
+            
+        }
+        else
+        x=y;
+    }
+    return 0;
+    
+}
+
